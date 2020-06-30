d8c492231cbf02ec3839c48abedd3d0ea722f22d
Ivan <imataras@yandex.ru>
2020-06-30 20:44:37 +0300

    add stack test

--- chapter-4/src/test/java/ru/ivanmataras/education/chapter4/TestStack.java
+++ chapter-4/src/test/java/ru/ivanmataras/education/chapter4/TestStack.java
@@ -1,4 +1,27 @@
 package ru.ivanmataras.education.chapter4;
 
+import org.junit.jupiter.api.*;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.TestMethodOrder;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class TestStack {
+
+    @Test
+    @BeforeAll
+    static void testCreateStackAndPushValues() {
+
+        Stack stack = new Stack(4);
+        stack.push(20);
+        stack.push(40);
+        stack.push(60);
+        stack.push(80);
+
+        assertNotNull(stack);
+
+    }
+
 }
