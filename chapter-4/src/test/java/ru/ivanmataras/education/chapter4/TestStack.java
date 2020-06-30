8697312749a33eced1a2e714e5a26ddcfb19dc90
Ivan <imataras@yandex.ru>
2020-06-30 21:20:54 +0300

    update stack test

--- chapter-4/src/test/java/ru/ivanmataras/education/chapter4/TestStack.java
+++ chapter-4/src/test/java/ru/ivanmataras/education/chapter4/TestStack.java
@@ -5,7 +5,12 @@ import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.TestMethodOrder;
 
+import static java.lang.System.out;
+
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class TestStack {
@@ -14,13 +19,31 @@ public class TestStack {
     @BeforeAll
     static void testCreateStackAndPushValues() {
 
-        Stack stack = new Stack(4);
+        Stack stack = new Stack(10);
         stack.push(20);
         stack.push(40);
         stack.push(60);
         stack.push(80);
 
         assertNotNull(stack);
+
+    }
+
+    @Test
+    @Order(1)
+    void testCreateStackAndPushAndPopValues() {
+
+        Stack stack = new Stack(10);
+        stack.push(20);
+        stack.push(40);
+        stack.push(60);
+        stack.push(80);
+
+        while (!stack.isEmpty()) {
+            int value = stack.pop();
+        }
+
+        assertTrue(stack.isEmpty());
 
     }
 
